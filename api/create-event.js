59808c61ba00159da6eb8c3786fdeca093a2b73b
Benjamin-synkro <chevalierbj@gmail.com>
2025-10-19 23:06:34 +0200

    Update create-event.js

--- api/create-event.js
+++ api/create-event.js
@@ -57,7 +57,7 @@ export default async function handler(req, res) {
         organizerEmail: eventData.organizerEmail || '',
         location: eventData.location || '',
         expectedParticipants: eventData.expectedParticipants || 0,
-        dates: eventData.dates,
+        dates: JSON.stringify(eventData.dates),
         participants: JSON.stringify([]),
         totalResponded: 0,
         status: 'active'
